5e0d5dccefe6ab739a245de0f5b9e25622aa002a
Carlos Acevedo <cacevedod@devco.com.co>
2019-10-11 10:03:27 -0500

    ajuste pipeline sonar

--- vars/multibranchJavaTemplate.groovy
+++ vars/multibranchJavaTemplate.groovy
@@ -44,18 +44,7 @@ def call(body) {
                     }
                     stage('static analysis') {
                         steps{
-                            withSonarQubeEnv('SonarQubeServer') {
-                                script{
-                                    sh "./gradlew sonarqube"
-
-                                    timeout(time: 1, unit: 'HOURS') {
-                                        def qg = waitForQualityGate()
-                                        if (qg.status != 'OK') {
-                                            error "Pipeline aborted due to quality gate failure: ${qg.status}"
-                                        }
-                                    }
-                                }
-                            }
+                            echo "STATIC ANALYSIS en: ${env.ENVIRONMENT} para ${env.ARTIFACT_NAME}"
 
                         }
                     }
